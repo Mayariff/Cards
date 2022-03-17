b1572dbe0735e6c53f455d13af9da06b541af2e1
Maya <79027972026@yandex.ru>
2022-03-17 16:50:40 +0500

    add Environments

--- src/dal/cardsApi.ts
+++ src/dal/cardsApi.ts
@@ -2,9 +2,10 @@ import axios, {AxiosResponse} from "axios";
 import {CardType} from "../bll/reducers/cardReducer";
 
 const instance = axios.create({
-    baseURL: 'https://neko-back.herokuapp.com/2.0',
+    //baseURL: 'https://neko-back.herokuapp.com/2.0',
     // baseURL: 'http://localhost:7542/2.0',
     withCredentials: true,
+    baseURL: process.env.REACT_APP_BASE_URL||'http://localhost:7542/2.0',
 })
 
 export const cardsApi = {
--- src/dal/mainAPI.ts
+++ src/dal/mainAPI.ts
@@ -1,9 +1,10 @@
 import axios, {AxiosResponse} from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://neko-back.herokuapp.com/2.0',
+    //baseURL: 'https://neko-back.herokuapp.com/2.0',
     // baseURL: 'http://localhost:7542/',
     withCredentials: true,
+    baseURL: process.env.REACT_APP_BASE_URL||'http://localhost:7542/2.0',
 })
 
 
